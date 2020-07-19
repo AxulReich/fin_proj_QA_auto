b63b19af75c297f1cdee45e01ff439188cb1da34
AxulReich <38258547+AxulReich@users.noreply.github.com>
2020-07-19 17:10:01 +0300

    Add constants for tests (#1)

---
+++ constants.py
@@ -0,0 +1,8 @@
+# from os import getenv
+
+# BASE_URL = getenv('BASE_URL',"http://selenium1py.pythonanywhere.com/")
+BASE_URL = "http://selenium1py.pythonanywhere.com/"
+
+IMPLICIT_WAIT = 2
+EXPLICIT_WAIT = 4
+WINDOW_SIZE = ('1280', '1024')
